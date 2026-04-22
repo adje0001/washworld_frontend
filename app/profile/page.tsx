01512e59c84ce3d6beeec327e11b9a3e01a9eaa6
Adam <adje0001@stud.kea.dk>
2026-04-22 15:38:45 +0200

    profile virker

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,17 +1,18 @@
 "use client";
+
 import { useQuery } from "@tanstack/react-query";
 
-// const token = localStorage.getItem("jwt");
-
-// const { data: user, isLoading } = useQuery({
-//   queryKey: ["profile"],
-//   queryFn: () =>
-//     fetch("api/profile", {
-//       headers: { Authorization: `Bearer ${token ?? ""}` },
-//     }).then((res) => res.json()),
-//   staleTime: 60 * 1000,
-// });
-
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const token = localStorage.getItem("jwt");
+
+  const { data: user } = useQuery({
+    queryKey: ["profile"],
+    queryFn: () =>
+      fetch("/api/profile", {
+        headers: { Authorization: `Bearer ${token ?? ""}` },
+      }).then((res) => res.json()),
+    staleTime: 60 * 1000,
+  });
+
+  return <h1>Profile page</h1>;
 }
